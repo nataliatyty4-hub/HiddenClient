9e9a1cb12e3fd83924da580e4b6a3d1eacc68ea5
HHH-Creator <nataliatyty4@gmail.com>
2026-03-13 20:50:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,75 @@
-const elements = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card");
 
-window.addEventListener("scroll", () => {
+window.addEventListener("scroll",()=>{
 
-elements.forEach(el => {
+cards.forEach(card=>{
 
-const pos = el.getBoundingClientRect().top;
-const screen = window.innerHeight;
+const top = card.getBoundingClientRect().top;
 
-if(pos < screen - 100){
-el.style.opacity = "1";
-el.style.transform = "translateY(0)";
+if(top < window.innerHeight-100){
+
+card.style.opacity = "1";
+card.style.transform = "translateY(0)";
+
 }
 
-});
+})
 
-});
+})
+
+
+
+function particles(){
+
+const canvas=document.createElement("canvas")
+
+document.getElementById("particles").appendChild(canvas)
+
+const ctx=canvas.getContext("2d")
+
+canvas.width=window.innerWidth
+canvas.height=window.innerHeight
+
+let p=[]
+
+for(let i=0;i<80;i++){
+
+p.push({
+
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+s:Math.random()*2
+
+})
+
+}
+
+function draw(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height)
+
+p.forEach(a=>{
+
+ctx.fillStyle="#00aaff"
+
+ctx.beginPath()
+
+ctx.arc(a.x,a.y,a.s,0,Math.PI*2)
+
+ctx.fill()
+
+a.y+=0.2
+
+if(a.y>canvas.height) a.y=0
+
+})
+
+requestAnimationFrame(draw)
+
+}
+
+draw()
+
+}
+
+particles()
