3990a34f59644267d6c46e85784fb9f6f988615c
HHH-Creator <nataliatyty4@gmail.com>
2026-03-13 20:58:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,19 @@
-const cards = document.querySelectorAll(".card");
+const canvas = document.getElementById("particles")
 
-window.addEventListener("scroll",()=>{
+const ctx = canvas.getContext("2d")
 
-cards.forEach(card=>{
-
-const top = card.getBoundingClientRect().top;
-
-if(top < window.innerHeight-100){
-
-card.style.opacity = "1";
-card.style.transform = "translateY(0)";
-
-}
-
-})
-
-})
-
-
-
-function particles(){
-
-const canvas=document.createElement("canvas")
-
-document.getElementById("particles").appendChild(canvas)
-
-const ctx=canvas.getContext("2d")
-
-canvas.width=window.innerWidth
-canvas.height=window.innerHeight
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
 let p=[]
 
-for(let i=0;i<80;i++){
+for(let i=0;i<100;i++){
 
 p.push({
 
 x:Math.random()*canvas.width,
 y:Math.random()*canvas.height,
-s:Math.random()*2
+r:Math.random()*2
 
 })
 
@@ -54,11 +29,11 @@ ctx.fillStyle="#00aaff"
 
 ctx.beginPath()
 
-ctx.arc(a.x,a.y,a.s,0,Math.PI*2)
+ctx.arc(a.x,a.y,a.r,0,Math.PI*2)
 
 ctx.fill()
 
-a.y+=0.2
+a.y+=0.3
 
 if(a.y>canvas.height) a.y=0
 
@@ -69,7 +44,3 @@ requestAnimationFrame(draw)
 }
 
 draw()
-
-}
-
-particles()
